ae2d286b56e4a78d50517c45104c72ba96c6a258
edaagunes <edagunes00@hotmail.com>
2024-09-30 20:15:28 +0300

    Created CariController and Cari's transactions

---
+++ MvcOnlineTicariOtomasyon/Controllers/CariController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Classes;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class CariController : Controller
+    {
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            var degerler = context.Carilers.Where(x => x.Durum == true).ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniCari()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniCari(Cariler p)
+        {
+            p.Durum = true;
+            context.Carilers.Add(p);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult CariSil(int id)
+        {
+            var cariler = context.Carilers.Find(id);
+            cariler.Durum = false;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult CariGetir(int id)
+        {
+            var cari = context.Carilers.Find(id);
+            return View("CariGetir", cari);
+        }
+
+        public ActionResult CariGuncelle(Cariler p)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("CariGetir");
+            }
+
+            else
+            {
+                var cari = context.Carilers.Find(p.CariID);
+                cari.CariAd = p.CariAd;
+                cari.CariSoyad = p.CariSoyad;
+                cari.CariSehir = p.CariSehir;
+                cari.CariMail = p.CariMail;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult MusteriSatis(int id) 
+        {
+            var degerler=context.SatisHarekets.Where(x=>x.CariID==id).ToList();
+            var cr = context.Carilers.Where(x=>x.CariID==id).Select(y=>y.CariAd + " "+y.CariSoyad).FirstOrDefault();
+            ViewBag.cari = cr;
+            return View(degerler);
+        }
+    }
+}
--- MvcOnlineTicariOtomasyon/Models/Classes/Cariler.cs
+++ MvcOnlineTicariOtomasyon/Models/Classes/Cariler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -12,10 +13,11 @@ namespace MvcOnlineTicariOtomasyon.Models.Classes
         [Key]
         public int CariID { get; set; }
         [Column(TypeName = "Varchar")]
-        [StringLength(30)]
+        [StringLength(30,ErrorMessage ="En fazla 30 karakter girebilirsiniz")]
         public string CariAd { get; set; }
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
+        [Required(ErrorMessage ="Soyad Giriniz")]
         public string CariSoyad { get; set; }
         [Column(TypeName = "Varchar")]
         [StringLength(13)]
@@ -23,6 +25,8 @@ namespace MvcOnlineTicariOtomasyon.Models.Classes
         [Column(TypeName = "Varchar")]
         [StringLength(50)]
         public string CariMail { get; set; }
+
+        public bool Durum { get; set; }
         public ICollection<SatisHareket> SatisHarekets { get; set; }
     }
 }
