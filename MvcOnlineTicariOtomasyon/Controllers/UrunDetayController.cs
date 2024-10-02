696342b829e9b7cc36bc50014162eb60e127044a
edaagunes <edagunes00@hotmail.com>
2024-10-02 22:31:34 +0300

    Added UrunDetayController and Detay class

---
+++ MvcOnlineTicariOtomasyon/Controllers/UrunDetayController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Classes;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class UrunDetayController : Controller
+    {
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            Class1 cs = new Class1();
+            // var degerler=context.Uruns.Where(x=>x.UrunID==1).ToList();
+            cs.Deger1=context.Uruns.Where(x=>x.UrunID== 1).ToList();
+            cs.Deger2=context.Detays.Where(y=>y.DetayID== 1).ToList();
+            return View(cs);
+        }
+    }
+}
--- MvcOnlineTicariOtomasyon/Models/Classes/Context.cs
+++ MvcOnlineTicariOtomasyon/Models/Classes/Context.cs
@@ -7,17 +7,18 @@ using System.Data.Entity;
 
 namespace MvcOnlineTicariOtomasyon.Models.Classes
 {
-    public class Context:DbContext
+    public class Context : DbContext
     {
-        public  DbSet<Admin> Admins{ get; set; }
-        public  DbSet<Cariler> Carilers{ get; set; }
-        public  DbSet<Departman> Departmans{ get; set; }
-        public  DbSet<FaturaKalem> FaturaKalems{ get; set; }
-        public  DbSet<Faturalar> Faturalars{ get; set; }
-        public  DbSet<Gider> Giders{ get; set; }
-        public  DbSet<Kategori> Kategoris{ get; set; }
-        public  DbSet<Personel> Personels{ get; set; }
-        public  DbSet<SatisHareket> SatisHarekets{ get; set; }
-        public  DbSet<Urun> Uruns{ get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Cariler> Carilers { get; set; }
+        public DbSet<Departman> Departmans { get; set; }
+        public DbSet<FaturaKalem> FaturaKalems { get; set; }
+        public DbSet<Faturalar> Faturalars { get; set; }
+        public DbSet<Gider> Giders { get; set; }
+        public DbSet<Kategori> Kategoris { get; set; }
+        public DbSet<Personel> Personels { get; set; }
+        public DbSet<SatisHareket> SatisHarekets { get; set; }
+        public DbSet<Urun> Uruns { get; set; }
+        public DbSet<Detay> Detays { get; set; }
     }
 }
